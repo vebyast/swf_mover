9ba882d71984370a7d2058341240018f278b275b
Vebyast <vebyast@gmail.com>
2021-01-17 17:24:41 -0800

    Finally figure out _exactly_ how MoveTo and such work

--- makefile
+++ makefile
@@ -16,9 +16,10 @@ integration:
 	$(RUN) python -m swf_mover.swf_mover --swf=testdata/avatar05_SkirtRight17.swf
 	$(EXPORTER) testdata/avatar05_SkirtRight17.swf testdata/avatar05_SkirtRight17.png
 	$(EXPORTER) testdata/avatar05_SkirtRight17.moved.swf testdata/avatar05_SkirtRight17.moved.png
-	$(MOGRIFY) -transparent white -trim testdata/avatar05_SkirtRight17.png
-	$(MOGRIFY) -transparent white -trim testdata/avatar05_SkirtRight17.moved.png
+	$(CONVERT) -transparent white -trim testdata/avatar05_SkirtRight17.png testdata/avatar05_SkirtRight17.processed.png
+	$(CONVERT) -transparent white -trim testdata/avatar05_SkirtRight17.moved.png testdata/avatar05_SkirtRight17.moved.processed.png
 	-$(COMPARE) -compose src testdata/avatar05_SkirtRight17.png testdata/avatar05_SkirtRight17.moved.png testdata/avatar05_SkirtRight17.diff.png
+	-$(COMPARE) -compose src testdata/avatar05_SkirtRight17.processed.png testdata/avatar05_SkirtRight17.moved.processed.png testdata/avatar05_SkirtRight17.processed.diff.png
 
 setup:
 	pip3 install --user poetry
--- swf_mover/swf_mover.py
+++ swf_mover/swf_mover.py
@@ -32,10 +32,17 @@ class Point(object):
     def __add__(self, other):
         return Point(x=self.x + other.x, y=self.y + other.y)
 
+    def __mul__(self, other):
+        return Point(x=self.x * other, y=self.y * other)
+
 
 HUGE_POINT = Point(float("+inf"), float("+inf"))
+NEGATIVE_HUGE_POINT = HUGE_POINT * -1
 
-HEADER = ("Header",)
+HEADER = (
+    "swf",
+    "Header",
+)
 ROOT_POS = HEADER + ("size", "Rectangle")
 TAGS = HEADER + ("tags",)
 DEFSHAPE = TAGS + ("DefineShape",)
@@ -48,28 +55,50 @@ CURVETO = ("CurveTo",)
 
 
 class MoveRootPositionVisitor(object):
-    def __init__(self, dx, dy):
-        self.dx = dx
-        self.dy = dy
+    def __init__(self, movement):
+        self.movement = movement
 
-    def Visit(self, element, path=()):
-        if path == ROOT_POS:
-            element.set("left", str(float(element.get("left")) + self.dx))
-            element.set("right", str(float(element.get("right")) + self.dx))
-            element.set("top", str(float(element.get("top")) + self.dy))
-            element.set("bottom", str(float(element.get("bottom")) + self.dy))
+    def Visit(self, element, parent_path=()):
+        path = parent_path + (element.tag,)
+
+        if (
+            path == ROOT_POS
+            or path == DEFSHAPE + SHAPE_POS
+            or path == DEFSHAPE2 + SHAPE_POS
+        ):
+            old_tl = Point.FromElement(element, "left", "top")
+            old_br = Point.FromElement(element, "right", "bottom")
+            new_tl = old_tl + self.movement
+            new_br = old_br + self.movement
+            element.set("left", str(new_tl.x))
+            element.set("right", str(new_br.x))
+            element.set("top", str(new_tl.y))
+            element.set("bottom", str(new_br.y))
+
+        if (
+            path == DEFSHAPE + EDGES + SHAPESETUP
+            or path == DEFSHAPE2 + EDGES + SHAPESETUP
+        ):
+            old = Point.FromElement(element)
+            new = old + self.movement
+            if element.get("x") is not None:
+                element.set("x", str(new.x))
+            if element.get("y") is not None:
+                element.set("y", str(new.y))
+
         else:
             for child in element:
-                self.Visit(child, path + (child.tag,))
+                self.Visit(child, path)
 
 
-class GetTopLeftVisitor(object):
+class GetBoundsVisitor(object):
     def __init__(self):
         self.tree_origin = None
         self.shape_origin = None
-        self._current_pt = None
+        self._current_pt = Point(0, 0)
 
         self.top_left = HUGE_POINT
+        self.bottom_left = NEGATIVE_HUGE_POINT
 
     def RegisterPoint(self, pt):
         self.top_left = Point.min(self.top_left, pt)
@@ -83,35 +112,39 @@ class GetTopLeftVisitor(object):
         self.RegisterPoint(value)
         self._current_pt = value
 
-    def Visit(self, element, path=()):
+    def Visit(self, element, parent_path=()):
+        path = parent_path + (element.tag,)
+
         if path == ROOT_POS:
             self.tree_origin = Point.FromElement(element, "left", "top")
             self.top_left = Point.min(self.top_left, self.tree_origin)
-        elif path == DEFSHAPE2 + SHAPE_POS:
-            pt_rel_to_origin = Point.FromElement(element, "left", "top")
-            self.shape_origin = pt_rel_to_origin + self.tree_origin
-        elif path == DEFSHAPE + EDGES + SHAPESETUP:
-            pt_rel_to_shape = Point.FromElement(element)
-            self.current_pt = pt_rel_to_shape + self.shape_origin
-        elif path == DEFSHAPE + EDGES + LINETO:
-            self.current_pt += Point.FromElement(element)
-        elif path == DEFSHAPE + EDGES + CURVETO:
-            self.current_pt += Point.FromElement(element, "x2", "y2")
 
-        elif path == DEFSHAPE2 + SHAPE_POS:
-            pt_rel_to_origin = Point.FromElement(element, "left", "top")
-            self.shape_origin = pt_rel_to_origin + self.tree_origin
-        elif path == DEFSHAPE + EDGES + SHAPESETUP:
-            pt_rel_to_shape = Point.FromElement(element)
-            self.current_pt = pt_rel_to_shape + self.shape_origin
-        elif path == DEFSHAPE + EDGES + LINETO:
+        elif path == DEFSHAPE + SHAPE_POS or path == DEFSHAPE2 + SHAPE_POS:
+            self.shape_origin = Point.FromElement(element, "left", "top")
+            self.current_pt = self.shape_origin
+
+        elif (
+            path == DEFSHAPE + EDGES + SHAPESETUP
+            or path == DEFSHAPE2 + EDGES + SHAPESETUP
+        ):
+            pt = Point.FromElement(element)
+            self.current_pt = pt + self.shape_origin
+
+        elif (
+            path == DEFSHAPE + EDGES + LINETO
+            or path == DEFSHAPE2 + EDGES + LINETO
+        ):
             self.current_pt += Point.FromElement(element)
-        elif path == DEFSHAPE + EDGES + CURVETO:
+
+        elif (
+            path == DEFSHAPE + EDGES + CURVETO
+            or path == DEFSHAPE2 + EDGES + CURVETO
+        ):
             self.current_pt += Point.FromElement(element, "x2", "y2")
 
         else:
             for child in element:
-                self.Visit(child, path + (child.tag,))
+                self.Visit(child, path)
 
 
 def main(argv):
@@ -127,14 +160,14 @@ def main(argv):
         xml = etree.parse(xml_in_file)
 
     # Find top-left corner
-    top_left_visitor = GetTopLeftVisitor()
-    top_left_visitor.Visit(xml.getroot())
-    logging.info(f"top_left: {top_left_visitor.top_left}")
+    bounds_visitor = GetBoundsVisitor()
+    bounds_visitor.Visit(xml.getroot())
+    logging.info(f"top_left: {bounds_visitor.top_left}")
 
     # Update root position to place everything in positive x, y
-    move_visitor = MoveRootPositionVisitor(
-        top_left_visitor.top_left.x, top_left_visitor.top_left.y
-    )
+    movement = bounds_visitor.top_left * -1
+    logging.info(f"movement: {movement}")
+    move_visitor = MoveRootPositionVisitor(movement)
     move_visitor.Visit(xml.getroot())
 
     # Recompile SWF
